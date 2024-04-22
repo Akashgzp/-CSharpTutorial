f4cf79578067f9b49fcf07312fb4617fcfcb7e74
Akashgzp <167341531+Akashgzp@users.noreply.github.com>
2024-04-22 10:17:38 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorial
+{
+    class A
+    {
+       public virtual  void show()
+        {
+            Console.WriteLine(" this is class A");
+        }
+    }
+
+    class B :A
+    {
+
+        public override void show() 
+        {
+            Console.WriteLine("This is class B");
+        }
+    }
+   
+    public class Program
+    {
+        static void Main(string [] args)
+        {
+            A b = new B();  
+            b.show();
+           
+        }
+    }
+}
